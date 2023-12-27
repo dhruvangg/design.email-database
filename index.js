fee38d9da824ee03a57ae41a10660a099ad356ec
dhruvang <dhruvangg@gmail.com>
2023-12-27 19:14:30 +0530

    Added template delete methods

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
-import { getTemplate, getTemplates, createTemplate, getTemplatesByQuery } from './services/Template.js'
+import {
+    getTemplate, getTemplates, getTemplatesByQuery,
+    createTemplate,
+    deleteTemplate, deleteTemplates
+} from './services/Template.js'
 import { createBrand } from './services/Brand.js'
 import { createCategory } from './services/Category.js'
 
-export { getTemplate, getTemplates, getTemplatesByQuery, createTemplate, createBrand, createCategory };
+export {
+    getTemplate, getTemplates, getTemplatesByQuery,
+    createTemplate, createBrand, createCategory,
+    deleteTemplate, deleteTemplates
+};
--- services/Template.js
+++ services/Template.js
@@ -22,24 +22,24 @@ const getTemplates = async (page = 1, limit = 4) => {
     try {
         await connectDB();
         const skip = (page - 1) * limit;
-        const templates = await Template.find().select({ "html": 0}).skip(skip).limit(limit).sort({createdAt: -1}).exec()
+        const templates = await Template.find().select({ "html": 0 }).skip(skip).limit(limit).sort({ createdAt: -1 }).exec()
         const count = await Template.countDocuments();
         return { page, limit, count, templates };
     } catch (error) {
         return { error: error.message };
-    } 
+    }
 }
 
 const getTemplatesByQuery = async (query) => {
     try {
         await connectDB();
-        const templates = await Template.find({ $text: { $search: query } }).select({ "html": 0}).sort({createdAt: -1}).exec()
+        const templates = await Template.find({ $text: { $search: query } }).select({ "html": 0 }).sort({ createdAt: -1 }).exec()
         return { query, templates };
     } catch (error) {
         return { error: error.message };
-    } 
+    }
 }
-  
+
 const getTemplate = async (id) => {
     try {
         await connectDB();
@@ -49,7 +49,33 @@ const getTemplate = async (id) => {
         return { success: false, error: error.message }
     }
 }
-export { createTemplate, getTemplates, getTemplate, getTemplatesByQuery }
+
+const deleteTemplate = async (id) => {
+    try {
+        await connectDB();
+        const data = await Template.findByIdAndDelete(id)
+        return { data }
+    } catch (error) {
+        return { success: false, error: error.message }
+    }
+}
+
+const deleteTemplates = async (ids) => {
+    try {
+        await connectDB();
+        const data = await Template.deleteMany({ _id: { $in: ids } });
+        return { success: true, data }
+    } catch (error) {
+        return { success: false, error: error.message }
+    }
+}
+
+
+export {
+    createTemplate,
+    getTemplates, getTemplate, getTemplatesByQuery,
+    deleteTemplate, deleteTemplates
+}
 
 
 
